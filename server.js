b2348c33477919c9229a73a0f91cd7bd87fe64c8
KaziHossain15 <kazhoss1@gmail.com>
2025-09-30 23:00:17 -0400

    created db.serialize() is a method that ensures SQL statements are executed in a strictly sequential order

--- server.js
+++ server.js
@@ -14,5 +14,25 @@ const db = new sqlite3.Database('./password_manager.db');
 
 // create tables
 db.serialize(() => {
-    
-    })
+    db.run(`CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password_hash TEXT NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    )`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS stored_passwords (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    service_name TEXT NOT NULL,
+    username TEXT NOT NULL,
+    encrypted_password TEXT NOT NULL,
+    url TEXT,
+    notes TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+  )`);
+});
+
